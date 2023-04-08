3cc7c6662146f5f9de4b20eb4f7b454ab0238a0d
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-07 21:20:43 -0400

    Objects

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-    let name = "Sandy";
-    let age = 34;
-    let isApproved = false;
-    let firstName = undefined;
-    let selectedColor = "brown";
 
-    console.log(age);
-    console.log(selectedColor);
-    console.log(name);// Prints Sandy
-    //Js is dynamically so the type of the variable can change @ runtime as oppose to Java where the TYPE of the variable has to stay the same 
-    name = 1;
-    console.log(name); // now prints 1 as oppose to Sandy
+    let person = {
+        //Add one or more K,V pairs
+        name: "Mosh",
+        ageOfMosh: 30
+    };
+    console.log(person);
+
+    //Dot Notation
+    person.name = 'John';
+    console.log(person);
+
+    //Bracket Notation
+    let selection = "name";
+    person[selection] = 'Mary';
+    console.log(person.name);
