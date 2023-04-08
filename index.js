65c3babde41b7f4978dc42dc0f4a8e8fb792bd9f
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-07 20:38:38 -0400

    Constants

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-    //Comment...which is just like Java... and is ignored..sigh...lol
-    let firstName = "Say";
-    let lastName = 'Wou';
-    console.log(firstName + " " + lastName);
+    // let allows us to reassign variables, unlike const
+    let interestRate = 0.3;
+    interestRate = 1;
+    console.log(interestRate);
+
+        // Results in uncaught TypeError 
+        const interestRateOne = 0.3;
+        interestRateOne = 1;
+        console.log(interestRateOne);
